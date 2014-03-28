44e0914dba1719f2a5b6c2f212be369473eb6fe0
MattCochrane <matthew.w.cochrane@gmail.com>
2014-03-27 22:07:54 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 
+// I/O Definitions
 #define CCWlim (PIND & (1 << PD0)) // Limit sensor input from board
 #define CWlim (PIND & (1 << PD1))
 #define ENABLE PD2	
@@ -14,114 +16,109 @@
 #define CLK PD6
 #define DIRECTION PD7
 
-volatile int Step = 0;
-volatile int Steplim = 0;
+#define Control PORTD
 
-int Timer(int Count);
+// State Definitions
+#define CW 0
+#define CCW 1
+#define BUSY 1
+#define DONE 0
+
+// Global Variables
+unsigned int TurnStepCount; // unsigned to allow more information
+unsigned char TurnInProgress, DirectionOfStep; // Indicators of movement
+
+// Function Prototypes
+void AbortTurn(void);
+
 void Calibrate(int dir);
-int Position(void);
+unsigned char Move(short NumberOfSteps, char MovingDirection, int Count);
 
-ISR(TIMER0_COMPA_vect){ // Goes to limit, forever
-	
-	PORTD ^= (1 << CLK); // Toggles CLK pin
-	
-	/*Step++;	// Counts steps, until steps reaches a set limit
-	if (Step >= (2*Steplim)) // Step limit here needs to be twice the set value bc there is two pulses to move once
-	{
-		Timer(0); // Turns off timer
-		Step = 0; // Resets values of step and step limit in case they are used later
-		Steplim = 0;
-	}*/
-		
-}
 
 int main(void)
 {
 	DDRD |= 0xFC; // Sets inputs and outputs. I need bits 1,0 as inputs so they are the only ones set to 0. 0xFC = 11111100
 	
-	PORTD &= ~(1 << ENABLE); // Sets enable low so that movement can occur
+	TurnInProgress = DONE; // Lets program know there is no turn happening right now
 	
 	sei(); // Enable global interrupts
 	
 	Calibrate (1); // Sets up defenders, MAYBE
-	
-	// Timer(200); // value inversely proportional to freq. Argument = (Period * Internal freq)/(2*Prescaler) - 1
-	
-	while(1) // TODO: explain
-	{
-		
-	}
-}
 
-int Timer(int Count) // The function that activates the interrupt
-{
-	if (Count == 0) // Turns timer off
+	while(1) // Loops forever
 	{
-		TCCR0B = 0;
 		
-		TIMSK0 = 0;
-		
-		OCR0A = 0;
-		
-		return 1;
-	}
-	else
-	{
-		TCCR0B |= (1 << WGM01); // Configure timer 0 to CTC
-		
-		TIMSK0 |= (1 << OCIE0A); // Enable CTC interrupt, interrupt occurs every time the value of the timer equals a set value 
-		
-		OCR0A = Count; // Set compare value to a compare rate, set value that is counted up to to activate interrupt7
-		
-		TCCR0B |= ((1 << CS02) || (1 << CS00)); // Prescale timer to 8MHz/1024
-		
-		return 1;
 	}
 }
 
 void Calibrate(int dir) // TODO
 {
-	while(1) // Moves until limit is reached
+	do 
 	{
-		PORTD |= (1 << DIRECTION); // Enables CCW motion
-				
-		Timer(200); // Steps defenders
-			
-		if (CCWlim != 0) // When CW limit is reached break the while loop
-		{
-			break; // break the while loop
-		}
-	}
-	while (1)
+		while(Move(100, CW, 4) == 1);
+	} while (CWlim == 0);
+	
+	do 
 	{
-		PORTD &= ~(DIRECTION); // Enables CW motion
-		
-		Timer(200); // Steps defenders
-		
-		if (CWlim != 0) // When CCW lim is reached break the while loop
-		{
-			break; // break the while loop
-		}
-	}
-		
+		while(Move(100,CCW, 4) == 1);
+	} while (CCWlim == 0);
 }
 
-/*int Position(void) // TODO: Move defender to desired positions
-{	
-	// TODO: move from sensor to sensor
-	if(CWlim)
+unsigned char Move(short NumberOfSteps, char MovingDirection, int Count)
+{
+	if (TurnInProgress == 1) // Don't move if already moving
 	{
-		PORTD |= (1 << DIRECTION);
-		// TODO: step 400
+		return 1;
 	}
-	else if(CCWlim)
+	
+	DirectionOfStep = MovingDirection; // Save direction of defender to global direction indicator
+	
+	if (MovingDirection == 1)
 	{
-		PORTD &= ~(1 << DIRECTION);
-		// TODO: step
+		Control = (1 << DIRECTION);
 	}
-	while(CWlim != 1)
+	else {
+		Control &= ~(1 << DIRECTION);
+	}
+	
+	TurnStepCount = NumberOfSteps;
+	
+	TurnInProgress = BUSY;
+	
+	TCCR0A = (1 << WGM01) | (1 << COM0A0); // Sets CTC mode, toggles CLK when 
+	
+	TCCR0B = (1 << CS02); // Prescale to 8MHz/256
+	
+	OCR0A = Count;		// Sets to 4ms pulse width : (period * 8MHz)/(2 * prescaler) - 1
+	
+	TCNT0 = 0; // Sets timer to zero
+	
+	Control &= ~(ENABLE); // Enables movement
+	
+	TIMSK0 = (1 << OCIE0A); // Enables interrupts, they take over
+	
+	return 0;
+}
+void AbortTurn(void)
+{
+	TIMSK0 &= ~(1 << OCIE0A); // Disable interrupts
+	
+	TurnStepCount = 0; // Clear the step count
+	
+	TCCR0B = 0; // Turn off timer
+	
+	Control |= (1 << ENABLE); // Disable movement
+	
+	TurnInProgress = DONE; // Tells program there is no turn
+}
+
+ISR(TIMER0_COMPA_vect)// Goes to limit, forever
+{ 
+	if ((CWlim != 0 && DirectionOfStep == CW) || (CCWlim != 0 && DirectionOfStep ==CCW) || --TurnStepCount == 0)
 	{
-		
+		TCCR0B = 0; // Disable timer until further notice
+		TIMSK0 &= ~(1 << OCIE0A); // Disable interrupts
+		Control |= (1 << ENABLE);
+		TurnInProgress = DONE;
 	}
 }
-*/
